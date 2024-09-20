4adc172b765323b32bd2b368322594561472525d
Swapnil Shinde <swapnilshinde9382@gmail.com>
2024-09-20 10:13:05 +0000

    device service validation

---
+++ src/validations/device.validation.ts
@@ -0,0 +1,42 @@
+import Joi from 'joi';
+
+const createDevice = {
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    country: Joi.string().required(),
+    region: Joi.string().required(),
+    city: Joi.string().required(),
+    category: Joi.string().required(),
+    manufacturer: Joi.string().required(),
+  })
+};
+
+const getDevices = {
+  query: Joi.object().keys({
+    userId: Joi.number().integer(), 
+    accountId: Joi.string(),
+    publicKey: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer()
+  })
+};
+
+const getDevice = {
+  params: Joi.object().keys({
+    deviceId: Joi.number().integer()
+  })
+};
+
+const deleteDevice = {
+  params: Joi.object().keys({
+    deviceId: Joi.number().integer()
+  })
+};
+
+export default {
+  createDevice,
+  getDevices,
+  getDevice,
+  deleteDevice
+};
