0f392bd19718120a3e214608de3eff87c35c8ec6
Swapnil Shinde <swapnilshinde9382@gmail.com>
2024-09-20 10:13:05 +0000

    notarization controllers

---
+++ src/controllers/notarize.controller.ts
@@ -0,0 +1,53 @@
+import httpStatus from 'http-status';
+import pick from '../utils/pick';
+import ApiError from '../utils/ApiError';
+import catchAsync from '../utils/catchAsync';
+import { deviceService, notarizedDataService } from '../services';
+import { randomUUID } from 'crypto';
+
+const createNotarization = catchAsync(async (req, res) => {
+    const {
+        deviceId,
+        meter_type,
+        time,
+        temprature,
+        totalEnergy,
+        today,
+        power,
+        apparentPower,
+        reactivePower,
+        factor,
+        voltage,
+        current,
+        raw
+    } = req.body;
+
+    const device = await notarizedDataService.createNotarization(
+        deviceId,
+        meter_type,
+        time,
+        temprature,
+        totalEnergy,
+        today,
+        power,
+        apparentPower,
+        reactivePower,
+        factor,
+        voltage,
+        current,
+        raw
+    );
+    res.status(httpStatus.CREATED).send(device);
+});
+
+const getNotarizations = catchAsync(async (req, res) => {
+    const filter = pick(req.query, ['deviceId', 'id']);
+    const options = pick(req.query, ['sortBy', 'limit', 'page']);
+    const result = await deviceService.queryDevices(filter, options);
+    res.send(result);
+});
+
+export default {
+    createNotarization,
+    getNotarizations,
+};
